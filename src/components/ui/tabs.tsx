d2c2d364f2b28b1ea44b577238dd23d856106752
Shashank Kumar <svsas005@gmail.com>
2025-10-21 16:29:21 +0530

    All pages changes

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center w-max whitespace-nowrap rounded-sm px-6 py-2 text-sm font-medium ring-offset-background transition-all data-[state=active]:bg-[#1E40AF] data-[state=active]:text-[#fff] data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      "inline-flex items-center justify-center w-max whitespace-nowrap rounded-sm px-6 py-2 text-sm font-medium ring-offset-background transition-all data-[state=active]:bg-primary data-[state=active]:text-[#fff] data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
       className,
     )}
     {...props}
