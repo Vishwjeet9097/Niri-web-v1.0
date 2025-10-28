365f1158d76c31dfb9310413b4071f19a6a4f96a
sarabjeet singh <130595952+sarabsingh1-6@users.noreply.github.com>
2025-10-28 11:56:45 +0530

    mospir approver dashboard

--- src/features/dashboard/components/reviewer/ReviewerSubmissionsTable.tsx
+++ src/features/dashboard/components/reviewer/ReviewerSubmissionsTable.tsx
@@ -53,6 +53,8 @@ export default function ReviewerSubmissionsTable() {
 
   const filteredSubmissions = submissions.filter((submission) => {
     // State filter
+
+    console.log(submission.stateUt, selectedState);
     const stateMatch = selectedState === "All" || submission.stateUt === selectedState;
     
     // Search filter
