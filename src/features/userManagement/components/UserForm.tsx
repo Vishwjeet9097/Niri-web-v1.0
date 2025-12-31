5acd6e51ba1ec036262b5a0f535043cc343a919e
ajay-skillmine <ajay.k@skill-mine.com>
2025-12-31 10:29:54 +0530

    User form edited

--- src/features/userManagement/components/UserForm.tsx
+++ src/features/userManagement/components/UserForm.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import MinistryIndicatorsSection from "./MinistryIndicatorsSection";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect, useCallback, useMemo, useRef } from "react";
 import { getRoleDisplayName } from "@/utils/roles";
