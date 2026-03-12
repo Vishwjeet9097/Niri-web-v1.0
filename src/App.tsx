be0b5988ef1625b37fd3951599ec73a0a4683328
seema973 <seema9730040@gmail.com>
2026-03-12 16:08:51 +0530

    changes done for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App = () => (
         <Sonner />
         <TestDataIndicator />
         <AuthProvider>
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.VITE_BASE_PATH?.replace(/\/$/, '') || undefined}>
             <Routes>
               <Route path="/login" element={<LoginPage />} />
               <Route
